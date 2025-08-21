d6974e65f9e064468bc9b4c76506e457b0d78f28
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-21 19:49:57 -0300

    Feito o index.php adicionando um background e 3 botões que levam para as abas do site.

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+include 'db.php'
+
+?>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seleção</title>
+    <link rel="stylesheet" href="../public/assets/style.css">
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main class="centralizar flex">
+<a href="../public/jogadores">
+<button class="botoes_menu">
+    <h1>Jogadores</h1>
+</button>
+</a>
+<a href="../public/times/">
+<button class="botoes_menu">
+    <h1>Times</h1>
+</button>
+</a>
+<a href="../public/partidas">
+    <button class="botoes_menu">
+        <h1>Partidas</h1>
+    </button>
+</a>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
