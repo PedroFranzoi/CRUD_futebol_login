aa13db83fb4b044688dbcb32cfde763fa0a7956e
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-22 09:51:09 -0300

    Arrumado a mensagem do delete ao impedir de deletar por haver jogadores dentro do time.

--- public/times/delete.php
+++ public/times/delete.php
@@ -1,10 +1,12 @@
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
     <title>Excluir times</title>
 </head>
+
 <body>
     <h2>Excluir time</h2>
     <p>Deseja realmente excluir o time?</p>
@@ -12,22 +14,27 @@
         <button type="submit">Excluir</button>
     </form>
     <a href="read.php">Voltar</a>
-    
-</body>
-</html>
-    <?php
-        include '../../config/db.php';
 
-        if(isset($_GET['id'])){
-            $id = $_GET['id'];
-            if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                $stmt = $conn->prepare("DELETE FROM times WHERE id = ?");
-                $stmt->bind_param("i", $id);
-                $stmt->execute();
-                header("Location: read.php");
-            }
-        }else{
-            header("Location: read.php");
+</body>
+
+</html>
+<?php
+include '../../config/db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $stmt = $conn->prepare("DELETE FROM times WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $stmt->close();
+        } catch (Exception $e) {
+            echo 'Não foi possível deletar o time, pois ainda há jogadores dentro dele.';
         }
-        
-    ?>
+    }
+} else {
+    header("Location: read.php");
+}
+
+?>
