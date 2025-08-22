08a87e8c26a21db3c5e60cf9b20610dc7dd9c0a6
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-08-21 21:55:14 -0300

    ADD: Simples leitura das partidas

---
+++ public/partidas/read.php
@@ -0,0 +1,39 @@
+<?php
+
+include '../../config/db.php';
+
+$sql = "SELECT timeFora.nome AS nomeCasa, timeCasa.nome AS nomeFora, data_jogo, gols_casa, gols_fora FROM partidas INNER JOIN times timeCasa ON partidas.time_casa_id = timeCasa.id INNER JOIN times timeFora ON partidas.time_fora_id = timeFora.id";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+
+    echo "<table border = '1'>
+        <tr>
+            <th> Time de Casa </th>
+            <th> Time de Fora </th>
+            <th> Data da Partida </th>
+            <th> Gols (Time de Casa) </th>
+            <th> Gols (Time de Fora) </th>
+        </tr>
+    ";
+
+    while($row = $result->fetch_assoc()){
+
+        echo "<tr>
+                <td> {$row['nomeCasa']} </td>
+                <td> {$row['nomeFora']} </td>
+                <td> {$row['data_jogo']} </td>
+                <td> {$row['gols_casa']} </td>
+                <td> {$row['gols_fora']} </td>
+            </tr>
+        ";
+    }
+    echo "</table>";
+}else{
+    echo "Nenhuma partida encontrada.";    
+}
+echo "<a href='create.php'>Registrar partida</a>";
+
+$conn -> close();
+
+?>
