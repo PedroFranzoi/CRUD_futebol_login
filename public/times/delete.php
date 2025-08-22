d02f155bd90dc230e19b3068836d50e96928bebc
davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-08-22 09:14:12 -0300

    add

--- public/times/delete.php
+++ public/times/delete.php
@@ -1,16 +1,33 @@
-<?php
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Excluir times</title>
+</head>
+<body>
+    <h2>Excluir time</h2>
+    <p>Deseja realmente excluir o time?</p>
+    <form action="" method="post">
+        <button type="submit">Excluir</button>
+    </form>
+    <a href="read.php">Voltar</a>
+    
+</body>
+</html>
+    <?php
+        include '../../config/db.php';
 
-include '../../config/db.php';
-
-$id = $_GET['id'];
-
-$sql = "DELETE FROM times WHERE id=$id";
-$sql = "SELECT * jogadores WHERE id <> '' ";
-
-if ($conn->query($sql) === true) {
-    echo "Time excluído com sucesso.";
-} else{
-     echo "Erro " . $sql . '<br>' . $conn->error;
-}
-$conn -> close();
-exit();
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $stmt = $conn->prepare("DELETE FROM times WHERE id = ?");
+                $stmt->bind_param("i", $id);
+                $stmt->execute();
+                header("Location: read.php");
+            }
+        }else{
+            header("Location: read.php");
+        }
+        
+    ?>
