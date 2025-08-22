739dbceeb48d4b4eda970a42b06f3d24dd1244e6
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-08-22 10:17:30 -0300

    ADD: update partidas

---
+++ public/partidas/update.php
@@ -0,0 +1,75 @@
+<?php
+include '../../config/db.php';
+
+$sqlTimes = "SELECT id, nome FROM times";
+$resultTimes = $conn->query($sqlTimes);
+$resultTimes2 = $conn->query($sqlTimes);
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $stmt = $conn->prepare("SELECT * FROM partidas WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    $dado = mysqli_fetch_assoc($res);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $idTimeCasa = $_POST["timeCasa"];
+    $idTimeFora = $_POST["timeFora"];
+    $dataPartida = $_POST["data"];
+    $golsCasa = $_POST["golsCasa"];
+    $golsFora = $_POST["golsFora"];
+
+    if ($idTimeCasa == $idTimeFora) {
+        echo 'Não é possível definir o mesmo time para Casa e para Fora!';
+    } else if ($golsCasa < 0 || $golsFora < 0) {
+        echo 'Não é possível definir valores negativos para a quantidade de gols!';
+    } else {
+        $id = $_GET['id'];
+        $stmt = $conn->prepare("UPDATE partidas SET time_casa_id = ?, time_fora_id = ?, data_jogo = ?, gols_casa = ?, gols_fora = ? WHERE id = ?");
+        $stmt->bind_param("iisiii", $idTimeCasa, $idTimeFora, $dataPartida, $golsCasa, $golsFora, $id);
+        if ($stmt->execute()) {
+            header("Location: read.php");
+        } else {
+            echo "Erro ao editar!";
+        }
+    }
+}
+?>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Registrar Partida</title>
+</head>
+
+<body>
+    <h2>Registrar Partida:</h2>
+    <form action="" method="post">
+        <label for="timeCasa">Time de Casa:</label>
+        <select name="timeCasa">
+            <?php while ($row = $resultTimes->fetch_assoc()): ?>
+                <option value="<?= $row['id'] ?>" <?php if($dado['time_casa_id'] == $row['id']){echo 'selected';} ?>><?= $row['nome'] ?></option>
+            <?php endwhile; ?>
+        </select><br>
+        <label for="timeFora">Time de Fora:</label>
+        <select name="timeFora">
+            <?php while ($row = $resultTimes2->fetch_assoc()): ?>
+                <option value="<?= $row['id'] ?>" <?php if($dado['time_fora_id'] == $row['id']){echo 'selected';} ?>><?= $row['nome'] ?></option>
+            <?php endwhile; ?>
+        </select><br>
+        <label for="data">Data da Partida:</label>
+        <input type="date" name="data" id="data" value="<?=$dado['data_jogo']?>"> <br>
+        <label for="golsCasa">Gols do time da Casa:</label>
+        <input type="number" name="golsCasa" id="golsCasa"  value="<?=$dado['gols_casa']?>"><br>
+        <label for="golsFora">Gols do time de Fora:</label>
+        <input type="number" name="golsFora" id="golsFora"  value="<?=$dado['gols_fora']?>"><br>
+        <button type="submit">Registrar Partida</button>
+    </form>
+
+</body>
+
+</html>
